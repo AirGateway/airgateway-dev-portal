fffd9b8c599e3d903cdba849e3b9aa13f2c41f52
Dmitry Fedorov <klka1@live.ru>
2018-01-29 20:09:35 +0300

    add more plan info output

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -14,10 +14,8 @@ if ($dashboardContainer.length) {
         url: host + urlMap.plans,
         success: function (response) {
             for (var i in response.data) {
-                if (response.data[i].activated) {
+                if (response.data[i].status == 'active') {
                     plans.push(response.data[i]);
-
-
                 }
             }
 
@@ -34,7 +32,7 @@ if ($dashboardContainer.length) {
         if (plans.length) {
             $dashboardContainer.prepend(tplDashboardPlanSelect({
                 plans: plans
-            }))
+            }));
 
             showPlan(plans[0].id);
         }
@@ -51,7 +49,9 @@ if ($dashboardContainer.length) {
                     rate: plans[i].rate,
                     per: plans[i].per,
                     quota_max: plans[i].quota_max,
-                    quota_renewal_rate: plans[i].quota_renewal_rate
+                    quota_renewal_rate: plans[i].quota_renewal_rate,
+                    url: plans[i].url,
+                    auth_header: plans[i].auth_header
                 }));
 
 
